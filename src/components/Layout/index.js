8fa5b885ae206930177e67630e77c70baec10f99
Deebo Jenkins <marc@weedrater.com>
2019-03-26 17:05:11 -0400

    Refactors to layout from index page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,76 +1,186 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Hidden from '@material-ui/core/Hidden'
+import Navigator from 'components/Navigator'
 
+import Header from 'components/Header'
 import { rhythm, scale } from 'utils/typography'
 
-class Layout extends React.Component {
-	render() {
-		const { location, title, children } = this.props
-		// eslint-disable-next-line no-undef
-		const rootPath = `${__PATH_PREFIX__}/`
-		let header
+let theme = createMuiTheme({
+	typography: {
+		useNextVariants: true,
+		h5: {
+			fontWeight: 500,
+			fontSize: 26,
+			letterSpacing: 0.5,
+		},
+	},
+	palette: {
+		primary: {
+			light: '#63ccff',
+			main: '#009be5',
+			dark: '#006db3',
+		},
+	},
+	shape: {
+		borderRadius: 8,
+	},
+})
 
-		if (location.pathname === rootPath) {
-			header = (
-				<h1
-					style={{
-						...scale(1.5),
-						marginBottom: rhythm(1.5),
-						marginTop: 0,
-					}}
-				>
-					<Link
-						style={{
-							boxShadow: `none`,
-							textDecoration: `none`,
-							color: `inherit`,
-						}}
-						to={`/`}
-					>
-						{title}
-					</Link>
-				</h1>
-			)
-		} else {
-			header = (
-				<h3
-					style={{
-						fontFamily: `Montserrat, sans-serif`,
-						marginTop: 0,
-					}}
-				>
-					<Link
-						style={{
-							boxShadow: `none`,
-							textDecoration: `none`,
-							color: `inherit`,
-						}}
-						to={`/`}
-					>
-						{title}
-					</Link>
-				</h3>
-			)
-		}
-		return (
-			<div
-				style={{
-					marginLeft: `auto`,
-					marginRight: `auto`,
-					maxWidth: rhythm(24),
-					padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-				}}
-			>
-				<header>{header}</header>
-				<main>{children}</main>
-				<footer>
-					© {new Date().getFullYear()}, Built with
-					{` `}
-					<a href="https://www.gatsbyjs.org">Gatsby</a>
-				</footer>
-			</div>
-		)
-	}
+theme = {
+	...theme,
+	overrides: {
+		MuiDrawer: {
+			paper: {
+				backgroundColor: '#18202c',
+			},
+		},
+		MuiButton: {
+			label: {
+				textTransform: 'initial',
+			},
+			contained: {
+				boxShadow: 'none',
+				'&:active': {
+					boxShadow: 'none',
+				},
+			},
+		},
+		MuiTabs: {
+			root: {
+				marginLeft: theme.spacing.unit,
+			},
+			indicator: {
+				height: 3,
+				borderTopLeftRadius: 3,
+				borderTopRightRadius: 3,
+				backgroundColor: theme.palette.common.white,
+			},
+		},
+		MuiTab: {
+			root: {
+				textTransform: 'initial',
+				margin: '0 16px',
+				minWidth: 0,
+				[theme.breakpoints.up('md')]: {
+					minWidth: 0,
+				},
+			},
+			labelContainer: {
+				padding: 0,
+				[theme.breakpoints.up('md')]: {
+					padding: 0,
+				},
+			},
+		},
+		MuiIconButton: {
+			root: {
+				padding: theme.spacing.unit,
+			},
+		},
+		MuiTooltip: {
+			tooltip: {
+				borderRadius: 4,
+			},
+		},
+		MuiDivider: {
+			root: {
+				backgroundColor: '#404854',
+			},
+		},
+		MuiListItemText: {
+			primary: {
+				fontWeight: theme.typography.fontWeightMedium,
+			},
+		},
+		MuiListItemIcon: {
+			root: {
+				color: 'inherit',
+				marginRight: 0,
+				'& svg': {
+					fontSize: 20,
+				},
+			},
+		},
+		MuiAvatar: {
+			root: {
+				width: 32,
+				height: 32,
+			},
+		},
+	},
+	props: {
+		MuiTab: {
+			disableRipple: true,
+		},
+	},
+	mixins: {
+		...theme.mixins,
+		toolbar: {
+			minHeight: 48,
+		},
+	},
 }
 
-export default Layout
+const drawerWidth = 256
+
+const styles = {
+	root: {
+		display: 'flex',
+		minHeight: '100vh',
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+	},
+	appContent: {
+		flex: 1,
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	mainContent: {
+		flex: 1,
+		padding: '48px 36px 0',
+		background: '#eaeff1',
+	},
+}
+
+function Layout({ location, title, children, classes }) {
+	// eslint-disable-next-line no-undef
+	const [mobileOpen, setMobileOpen] = useState(false)
+
+	return (
+		<MuiThemeProvider theme={theme}>
+			<div className={classes.root}>
+				<CssBaseline />
+				<nav className={classes.drawer}>
+					<Hidden smUp implementation="js">
+						<Navigator
+							PaperProps={{ style: { width: drawerWidth } }}
+							variant="temporary"
+							open={mobileOpen}
+							onClose={() => setMobileOpen(!mobileOpen)}
+						/>
+					</Hidden>
+					<Hidden xsDown implementation="css">
+						<Navigator PaperProps={{ style: { width: drawerWidth } }} />
+					</Hidden>
+				</nav>
+				<div className={classes.appContent}>
+					<Header onDrawerToggle={() => setMobileOpen(!mobileOpen)} />
+					<main className={classes.mainContent}>{children}</main>
+				</div>
+			</div>
+		</MuiThemeProvider>
+	)
+}
+
+Layout.propTypes = {
+	classes: PropTypes.object.isRequired,
+}
+export default withStyles(styles)(Layout)
