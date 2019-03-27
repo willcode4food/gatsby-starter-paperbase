704c61f4623f241c742a4b6e20ff340ea7fab6a6
Deebo Jenkins <marc@weedrater.com>
2019-03-27 09:56:26 -0400

    prettied some boilerplate

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,12 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography'
+import Wordpress2016 from 'typography-theme-wordpress-2016'
 
 Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
+	return {
+		'a.gatsby-resp-image-link': {
+			boxShadow: `none`,
+		},
+	}
 }
 
 delete Wordpress2016.googleFonts
@@ -15,7 +15,7 @@ const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+	typography.injectStyles()
 }
 
 export default typography
