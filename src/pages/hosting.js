ce1beadf276a38affc352f860c2cedfbf54368c8
Marc Arbesman <marbesman@gmail.com>
2020-01-23 21:03:17 -0500

    Upgrades all packages to latest

--- src/pages/hosting.js
+++ src/pages/hosting.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from 'components/Layout'
 import Content from 'components/Content'
+
 function HostingPage({ location }) {
 	const pageTitle = location ? location.pathname.replace(/\//g, '') : ''
 	return (
